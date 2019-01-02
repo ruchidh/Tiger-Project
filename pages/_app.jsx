d2301a9eaa42c02adb88d08623094e85a18dae54
ruchidh <ruchi492@gmail.com>
2019-01-02 16:32:30 +0530

    _app cut shot

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,60 +2,36 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 import App, { Container } from 'next/app';
-
 import Head from 'next/head';
 import Router from 'next/router';
 import withRedux from 'next-redux-wrapper';
 
-import HowItWorks from 'components/v2/How-It-Works';
-
 import initStore from 'store';
-import { routeActions } from 'store/route/actions';
-import { profileActions } from 'store/profile/actions';
-import { getConstants } from 'store/constants';
-import { AppActions } from 'store/app/actions';
-import { setStoreState as setFeedbackStoreState } from 'store/feedback/actions';
-
-import getLayout from 'components/Layout';
-import Offline from 'components/Offline';
-
-import { roles } from 'util/navRoutes';
-
-import getRouteData from 'util/app-helpers/route-data';
-import { getCookie } from 'util/app-helpers/cookies';
-
-import initSocket from 'util/app-helpers/socket';
-import initFormToObject from 'util/app-helpers/form-to-object';
-
-import handleRolesValidation from 'util/app-helpers/roles-validation';
-import handleRouteChangeEvents from 'util/app-helpers/route-change-events';
-
-import identifyUser from 'util/analytics/identify';
+import { setStoreState } from 'store/route/actions';
+import * as Progress from 'util/progress';
 import 'react-dates/initialize';
 
 class MyApp extends App {
 	static async getInitialProps({ Component: { getInitialProps }, ctx }) {
 		const {
-			store, isServer, pathname, query = {},
+			store, pathname, query = {},
 		} = ctx;
 
-		store.dispatch(setStoreState({ pathname, query, asPath }));
+		store.dispatch(setStoreState({ pathname, query }));
 
 		const pageProps = getInitialProps ? await getInitialProps({ ...ctx }) : {};
 
-		const pageProps = {
-			pathname,
-			query,
-			...(initialProps || {}),
+		return {
+			pageProps: {
+				...pageProps,
+				pathname,
+				query,
+			},
 		};
-
-		return { pageProps };
 	}
 
 	render() {
 		const { Component, store, pageProps } = this.props;
-		const { pathname, cookie } = pageProps || {};
-
 		return (
 			<Container>
 				<Head>
@@ -68,5 +44,14 @@ class MyApp extends App {
 		);
 	}
 }
+
+Router.onRouteChangeStart = () => {
+	Progress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+	Progress.stop();
+};
+
 
 export default withRedux(initStore)(MyApp);
